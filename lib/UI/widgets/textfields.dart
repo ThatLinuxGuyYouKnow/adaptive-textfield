1ec8affbc9071de62eab78069db3c21478c7fbba
Emmanuels-code <arocket04@gmail.com>
2025-02-26 22:50:04 +0100

    logic and UI separation

---
+++ lib/UI/widgets/textfields.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainTextField extends StatelessWidget {
+  MainTextField({super.key});
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+    );
+  }
+}
