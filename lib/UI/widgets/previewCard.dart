46a53a1c31574183c899594557350d7622a61dd9
Emmanuels-code <arocket04@gmail.com>
2025-02-27 19:24:11 +0100

    chore: new card for task preview

--- lib/UI/widgets/previewCard.dart
+++ lib/UI/widgets/previewCard.dart
@@ -14,3 +14,18 @@ class EventPreviewCard extends StatelessWidget {
     );
   }
 }
+
+class TaskPreviewCard extends StatelessWidget {
+  TaskPreviewCard({super.key});
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: 700,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+          border: Border.all(color: Colors.pink.withOpacity(0.1), width: 2),
+          boxShadow: [BoxShadow(color: Colors.pink.withOpacity(0.2))]),
+    );
+  }
+}
