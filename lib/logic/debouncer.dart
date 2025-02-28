2f2ed9bdb6a12f9f553f1c9eb4af54f9127f07a3
Emmanuels-code <arocket04@gmail.com>
2025-02-28 10:44:20 +0100

    feature: added debouncer

---
+++ lib/logic/debouncer.dart
@@ -0,0 +1,18 @@
+// debouncer.dart
+import 'dart:async';
+
+class Debouncer {
+  final Duration delay;
+  Timer? _timer;
+
+  Debouncer({required this.delay});
+
+  void call(void Function() callback) {
+    _timer?.cancel();
+    _timer = Timer(delay, callback);
+  }
+
+  void dispose() {
+    _timer?.cancel();
+  }
+}
